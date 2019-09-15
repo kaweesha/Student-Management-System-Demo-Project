00ee3e75c24dff9ca834891bfc8e53b530789a31
kaweesha <wkaweesh@gamil.com>
2019-09-15 21:18:25 +0530

    update sql file

--- create_database_and_table.sql
+++ create_database_and_table.sql
@@ -10,13 +10,26 @@ CREATE TABLE `students` (
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email_address` varchar(45) DEFAULT NULL,
+  `class_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `class_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+INSERT INTO `teachers` VALUES
+	(1, 'A. C. Perera', '8A', 'aperera@gmail.com'),
+	(2, 'N. P. De Silva', '8B', 'ndesilva@gmail.com');
 
 INSERT INTO `students` VALUES 
-	(1,'David','Adams','david@gmail.com'),
-	(2,'John','Doe','john@gmail.com'),
-	(3,'Ajay','Rao','ajay@gmail.com'),
-	(4,'Mary','Public','mary@gmail.com'),
-	(5,'Maxwell','Dixon','max@gmail.com');
+	(1,'David','Adams','david@gmail.com','8A'),
+	(2,'John','Doe','john@gmail.com','8A'),
+	(3,'Ajay','Rao','ajay@gmail.com','8A'),
+	(4,'Mary','Public','mary@gmail.com','8B'),
+	(5,'Maxwell','Dixon','max@gmail.com','8B');
+	(6,'Tony','Bill','t.bill@gmail.com','8B');
